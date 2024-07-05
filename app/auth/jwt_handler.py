53f99f32ba6f170dea156be76b3586bbd6630f92
ajmalniaz <81761465+ajmalniaz@users.noreply.github.com>
2024-07-05 07:32:55 +0100

    Create jwt_handler.py

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,35 @@
+# Tfor signing , encoding , decoding and returning JWTS
+import time
+from typing import Dict
+
+import jwt
+from decouple import config
+
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+def token_response(token: str):
+    return {
+        "access_token": token
+    }
+
+# function used for signing the JWT string
+def signJWT(teacher_id: str) -> Dict[str, str]:
+    payload = {
+        "teacher_id": teacher_id,
+        "expires": time.time() + 10000
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)
+
+
+def decodeJWT(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {}
+
